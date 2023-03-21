2be1b21b41276d0dde0785706c841844c08074bd
Shefaya <shefayaparwin@gmail.com>
2023-03-21 12:39:47 +0530

    Reverted handling to default color as transparent to match with RN web

--- skia/RSkComponentRNSVGSvgView.cpp
+++ skia/RSkComponentRNSVGSvgView.cpp
@@ -53,9 +53,6 @@ void RSkComponentRNSVGSvgView::OnPaint(SkCanvas *canvas) {
   drawBackground(canvas,frame,borderMetrics,viewProps.backgroundColor);
   drawBorder(canvas,frame,borderMetrics,viewProps.backgroundColor);
 
-  // Fix: Altering Skia's defult paint behaviour to match with reference platform.
-  alterSkiaDefaultPaint();
-
   //3. Start render from Root SVG Node
   RNS_LOG_DEBUG("---Start render from Root SVG Node---");
   SkSVGIDMapper    dummyIDMapper; // Deprecated, will be using RSkSVGIDMapper
@@ -156,32 +153,6 @@ void RSkComponentRNSVGSvgView::onSetAttribute(SkSVGAttribute attr, const SkSVGVa
       break;
     default:
       this->INHERITED::onSetAttribute(attr, attrValue);
-  }
-}
-
-inline void RSkComponentRNSVGSvgView::alterSkiaDefaultPaint() {
-//Note: Skia's default paint is black. WhereIn on SVG Reference platform it's transparent
-//      To Match with Reference output, resetting color to transparent, if color not speciifed.
-
-//      This is done only on outtermost group/level 1 parent's of svg container , to enable
-//      children to inherit undefined value from it's parent. And for the same reason it's been
-//      done once all the child elements are inserted to the SVGView.
-
-  for (auto &item:childRSkNodeList_) {
-    RNS_LOG_DEBUG(" Child Tag : "<<(int)item.second->tag());
-    if(item.second->tag() == SkSVGTag::kG) {
-      auto componentNode=dynamic_cast<RSkComponentRNSVGGroup *>(item.second);
-      if(componentNode) {
-        auto const &newRNSVGGroupPropsProps = *std::static_pointer_cast<RNSVGGroupProps const>(componentNode->getComponentData().props);
-        // Reset Stroke & Fill Color if not specifed to transparent
-        if(std::find (newRNSVGGroupPropsProps.propList.begin(), newRNSVGGroupPropsProps.propList.end(), "fill") == newRNSVGGroupPropsProps.propList.end()) {
-          componentNode->setPaintAttribute(SkSVGAttribute::kFill,"none");
-        }
-        if(std::find (newRNSVGGroupPropsProps.propList.begin(), newRNSVGGroupPropsProps.propList.end(), "stroke") == newRNSVGGroupPropsProps.propList.end()) {
-          componentNode->setPaintAttribute(SkSVGAttribute::kStroke,"none");
-        }
-      }
-    }
   }
 }
 
